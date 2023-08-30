b02011088e0f9637a2f60158f3ca714b07bb9ee6
sujine2 <twinsujin22@gmail.com>
2023-08-30 21:59:49 +0900

    Update address

--- packages/app/src/pages/Admin.tsx
+++ packages/app/src/pages/Admin.tsx
@@ -219,14 +219,14 @@ export const Admin = () => {
     let JubjubTemplateFactory: Jubjub__factory;
     let libs: JubjubLibraryAddresses;
     libs = {
-      ["contracts/poseidon/PoseidonT6.sol:PoseidonT6"]:
-        "0x6073Da004305af93A9deBa43F6490Df8ed6C43Ec",
-      ["contracts/poseidon/PoseidonT5.sol:PoseidonT5"]:
-        "0xE9334D82EF3a03A9677e1B7463B497B19aA2D5b0",
       ["contracts/poseidon/PoseidonT3.sol:PoseidonT3"]:
         "0x55B28e9b048BFFb6BF8d05b00E071C78450A429b",
       ["contracts/poseidon/PoseidonT4.sol:PoseidonT4"]:
         "0xf6c14e70D41f322B69Ada8C53daF9593aC165E08",
+      ["contracts/poseidon/PoseidonT5.sol:PoseidonT5"]:
+        "0xE9334D82EF3a03A9677e1B7463B497B19aA2D5b0",
+      ["contracts/poseidon/PoseidonT6.sol:PoseidonT6"]:
+        "0x6073Da004305af93A9deBa43F6490Df8ed6C43Ec",
     };
     JubjubTemplateFactory = new Jubjub__factory(libs, deployer);
 
