f715059d6de0451e3a5c5b4a5b378933bf95fcca
sujine2 <twinsujin22@gmail.com>
2023-09-03 13:43:27 +0900

    Update projects

--- packages/app/src/data.tsx
+++ packages/app/src/data.tsx
@@ -52,6 +52,235 @@ let projects = [
     id: "3",
     recipientId: 3,
   },
+  {
+    Timestamp: "9/3/2023 11:41:27",
+    projectName: "Trexx E-Sport Loyalties",
+    tagline: "Be friends with your E-sports idol",
+    description:
+      "We connect e-sports fans with their idols using engaging blockchain-powered loyalty and rewards programs to automatically measure and amplify each esports fan's passion, helping brands and teams build stronger, lasting connections. Our mission is to transform every fan's enthusiasm into a valuable asset, promoting customer loyalty and driving brand growth.",
+    ethereumAddress: "0x42A9b02Fb4E309DE5a54D850Caf2a8716c781f83",
+    website: "https://github.com/trexx-games/LoyaltyProgram",
+    thumbnailImageLink:
+      "https://drive.google.com/open?id=1R-uXVOdvHhw7grvknPAGeuI6LpkkQgbF",
+    logoCdnUrl:
+      "https://drive.google.com/open?id=1sgPXsk0HplJ1GhzWFTJv-0TPPajgZAI6",
+    bannerImageLink:
+      "https://drive.google.com/open?id=19iuiUEIoutuz3mLCoZCYqYpl32ywf7wM",
+    id: "4",
+    recipientId: 4,
+  },
+  {
+    Timestamp: "9/3/2023 12:08:17",
+    projectName: "Traffle",
+    tagline: "Fair and Simple NFT Raffle Platform",
+    description:
+      "Start a raffle with your NFT. The winner is fairly picked by Chainlink VRF.\nBest way to liquidate NFT under a day, and also giving chance of winning NFTs at <1% of price!",
+    ethereumAddress: "0x0A642B39595782FbeC9C75202eE6248E51008933",
+    website: "https://traffle.tracerofficial.com/",
+    thumbnailImageLink:
+      "https://drive.google.com/open?id=1QejOVV_AHHz-rEbrYRtwEvnN9EmLhqUx",
+    logoCdnUrl:
+      "https://drive.google.com/open?id=1qghHQ9F4wd_92sElLRt5QneHPOXkQl8e",
+    bannerImageLink:
+      "https://drive.google.com/open?id=126zqlEC05VzieORg5n2x4Qpr5o6Iy4kf",
+    id: "5",
+    recipientId: 5,
+  },
+  {
+    Timestamp: "9/3/2023 12:24:06",
+    projectName: "Hookbreaker",
+    tagline: "Accurate Phishing/Scam Website Detection",
+    description:
+      "Hookbreaker is a Chrome extension that analyzes a site of your choosing for signs of phishing. It uses Indicators of Attack (IoAs), known patterns of phishing websites, to check if the site you are viewing is likely a scam.\n\nHookbreaker does more than existing anti-phishing extensions, as it analyzes HTML content and external script links, not just the website\u2019s domain (which it does too). The extension only checks the current tab\u2019s site if you click the extension\u2019s icon, preserving privacy for websites that you are comfortable with.",
+    ethereumAddress: "0xd1fFdA9C225DDEE34f0837BF4D4a441bDd54C473",
+    website: "https://github.com/d0wnlore/hookbreaker",
+    thumbnailImageLink:
+      "https://drive.google.com/open?id=1oZVSzEw9jH2A7uELCNT8H_KQiGNUVFVt",
+    logoCdnUrl:
+      "https://drive.google.com/open?id=1I1InJ0kXgEibUtzhgVOZnU9qcFNrszrg",
+    bannerImageLink:
+      "https://drive.google.com/open?id=1t-HUn0WCmrCXl_bREyHOYbn6G1DnjLKp",
+    id: "6",
+    recipientId: 6,
+  },
+  {
+    Timestamp: "9/3/2023 12:24:21",
+    projectName: "ZK-mask",
+    tagline:
+      "Our project is getting personal facial scores by third parties using zk technology. Users could upload their picture for rating, wait for a certain epoch, and the score is finalized and written in ID.",
+    description:
+      "Our project is getting personal facial scores by third parties with zk-encrypted. Scoring method is held by decentralized and zk-secured way using UniRep built upon pse-Semaphore. When users upload their picture for rating, they should wait for certain epoch and gets finalized facial score. For use case, this could be used in dating chat application. In this situation, people could only enter the chatting room only if they meet specific scoring criteria which is guaranteed by ZK-proof.",
+    ethereumAddress: "0x035c628c9b835B22Da9a2Dd09d6f81691F4DC946",
+    website: "https://github.com/2023-Ethcon-jellybelly-zkMask",
+    thumbnailImageLink:
+      "https://drive.google.com/open?id=1FZN91Xwng9huDuCWY8uFN_0nVqUaj9nd",
+    logoCdnUrl:
+      "https://drive.google.com/open?id=1aoy-Db00tN7ORiOndAy39x-L-tG1pXqz",
+    bannerImageLink:
+      "https://drive.google.com/open?id=1cmHneE1WTQzXm96ZREcerRKXEmdcN4at",
+    id: "7",
+    recipientId: 7,
+  },
+  {
+    Timestamp: "9/3/2023 12:34:26",
+    projectName: "NFT Saka",
+    tagline:
+      "You can customize the image and mint your NFT on the OP network, then bridge the minted NFT between the OP and ETH networks.",
+    description:
+      "While there are services available for bridging ERC-20 tokens, there is currently a lack of solutions for bridging ERC-721 tokens between the OP and ETH networks. Saka NFT offers a solution to bridge your ERC-721 tokens seamlessly between the OP and ETH networks.",
+    ethereumAddress: "0x6eF61b3715c3Fa3483340C6d0f44a4CECD96A540",
+    website: "https://github.com/TralaTomJeong/Ethcon2023",
+    thumbnailImageLink:
+      "https://drive.google.com/open?id=1OW3YZUBRHbdCHkYOf6iM0BYODoSbd66h",
+    logoCdnUrl:
+      "https://drive.google.com/open?id=1VlDTLoirV5yH4Mq7dpeH7-v0f9ychF0O",
+    bannerImageLink:
+      "https://drive.google.com/open?id=1RnvNC4ABq3KCjTo0t0AQ6V3CjgdXPr60",
+    id: "8",
+    recipientId: 8,
+  },
+  {
+    Timestamp: "9/3/2023 12:34:26",
+    projectName: "torch2circom",
+    tagline: "Translating pytorch model to circom circuits",
+    description:
+      "Our project is to make bridge between ML and ZK, by giving efficient tools for ML developers to bring ZK features. We present torch2circom, which automatically translate pytorch model to circom circuits.",
+    ethereumAddress: "0x4036Ac48099e9B4685ecAdbD2A43874d5608aBE9",
+    website: "https://github.com/torch2circom",
+    thumbnailImageLink:
+      "https://drive.google.com/open?id=1IGp4T6TUJDWgfrh-BNqVoknmysmtgZHu",
+    logoCdnUrl:
+      "https://drive.google.com/open?id=1fXusNE9OR7VKF8tBP6e-FrLvOO5ZCEhH",
+    bannerImageLink:
+      "https://drive.google.com/open?id=1cuh3Pgue83mz7ae1orzmK3RyF07VvCOf",
+    id: "9",
+    recipientId: 9,
+  },
+  {
+    Timestamp: "9/3/2023 13:06:00",
+    projectName: "Paylink",
+    tagline:
+      "Paylink allows you to send a crypto payment to anyone with just a link.",
+    description:
+      "Paylink is a revolutionary tool that simplifies Web3 payments across multiple blockchains, not just Ethereum. Utilizing a multichain approach, we enable users to send and receive payments effortlessly through a simple link. What sets Paylink apart is our innovative use of account abstraction, making it exceptionally easy for recipients to claim their funds. This user-friendly interface lowers barriers to entry, making Web3 and decentralized finance (DeFi) accessible to a wider audience. Paylink contributes to the growth of blockchain ecosystems by driving user adoption, expanding use cases, fostering community engagement, and enhancing the overall experience",
+    ethereumAddress: "0xE27FBA2d264b8a12315b447d17d69a06FbaB3F90",
+    website: "https://github.com/EthCon2023",
+    thumbnailImageLink:
+      "https://drive.google.com/open?id=1KUocqDvwCWvycdG0GJfIQskj3udEbMLk",
+    logoCdnUrl:
+      "https://drive.google.com/open?id=1OFt_CGMqOfw7pPkR_cnLLXXgD4ylkD1K",
+    bannerImageLink:
+      "https://drive.google.com/open?id=1O476o4jd4DazV9D-oW8awbWCCTLCo-II",
+    id: "10",
+    recipientId: 10,
+  },
+  {
+    Timestamp: "9/3/2023 13:07:35",
+    projectName: "Careerzen",
+    tagline: "Business networking platform based on Career NFT",
+    description:
+      "Careerzen is a business networking service based on NFT profile representing user's career such as education, skills and awards etc by owning each career NFTs.",
+    ethereumAddress: "0x68BEeB607B96A7C5D177F58C74d593d654f71705",
+    website: "https://github.com/orgs/Careerzenn/repositories",
+    thumbnailImageLink:
+      "https://drive.google.com/open?id=1EYJPAhUhF2gy2jjOJ8ysCBk5_f5UkU6m",
+    logoCdnUrl:
+      "https://drive.google.com/open?id=1IG-pMDFfjHzo0-Su6GZZdofFHKi-VT18",
+    bannerImageLink:
+      "https://drive.google.com/open?id=1pq8yfzIxgsvYosympgRQ_dSpSzWLsCU7",
+    id: "11",
+    recipientId: 11,
+  },
+  {
+    Timestamp: "9/3/2023 12:48:18",
+    projectName: "Loanster",
+    tagline:
+      "Leverage or Hedge UniswapV3 based positions using Lending Protocols(Spark, AAVE, LineaBank) to maximize profits and mitigate Impermanent Loss and enjoy a super smooth UX in a single application.",
+    description:
+      "DeFi is one of the main pillars of the Ethereum Ecosystem. Uniswap V3, although being one of the most popular DeFi protocol, has major problems for lp providers. Loanster aims to solve the main problems these problems and give a better DeFi experience to LP providers. \n\nWhile Uniswap V3 addressed issues like wasted liquidity and price impacts through concentrated liquidity, it introduced a higher risk of impermanent loss for liquidity providers. In fact, over 50% of liquidity providers experience impermanent losses due to price fluctuations. \n\nFor example, in the case of investing in ETH+DAI position, if the ETH price goes up and the current price tick goes out of range, your position will change to 0% ETH and 100% DAI. In this case, you cannot gain any profit from the increase of the ETH price. Conversely, if ETH price drops, and your position changes to 100% ETH and 0% DAI, you will take all the losses directly. \n\nSo, Uniswap V3 can only profit in crab markets, when the price is stable and stays in a certain price range. In order to diversify our strategies, and be able to profit in bear or bull markets, we integrated the lending protocol in order to mitigate the impermanent losses. In a bullish market, we borrow DAI and deposit ETH as collateral. This way, we can take the benefits of the increase in the ETH price and still earn LP fees, maximizing our profits in the long strategy. In the bearish market, we do the exact opposite, by borrowing ETH and depositing DAI as collateral. Depending on the anticipated LP fees and anticipated price change, users could optimize profits by adjusting the borrowing rate.\n\nAlso, Uniswap V3 has a complex UX for preparing the exact amount of assets to add as liquidity for their position. Once they swap their assets in the pool, the ratio of their position will also change, so it is very difficult to put all of your desired assets into your position.\nLoanster solved this problem using the Loanster Contract, which calculates the exact amount to swap and add as liquidity for your position. \n\nAlso, defi infrastructures such as swaps and bridges are scattered, so you need to navigate through many different dApp pages. Loanster also aggregated these infrastructures and abstracted the complex process. It calculates the route for you, and all you need to do is press approve on a single page, and you\u2019re done investing! To summarize it all, people can enjoy a smooth UX and enjoy various investment strategies on a single page!",
+    ethereumAddress: "0xAA289325d1afc4AA040281b10dD9f10A8560D296",
+    website: "https://github.com/swimmiee/ethcon-korea-loanster/tree/main",
+    thumbnailImageLink:
+      "https://drive.google.com/open?id=19QofPp_GKy_JMKoRjDZOvFpwyyPKmzib",
+    logoCdnUrl:
+      "https://drive.google.com/open?id=1u1Tp3BvHKzLdMCLuopn1dy1K18afJuHW",
+    bannerImageLink:
+      "https://drive.google.com/open?id=1p8UZKCyZmHYv53Wg-d6R-SKGhfNi37BW",
+    id: "12",
+    recipientId: 12,
+  },
+  {
+    Timestamp: "9/3/2023 12:51:33",
+    projectName: "Chain Concierge",
+    tagline:
+      "Your personal AI assistant summarizes governance proposal requests for each protocol",
+    description:
+      "Keep track of all the governance proposals that come up on DAOs with our Chain Reporter, which summarizes the key points of the proposals in an instant. You can publish this as a newsletter by linking it to your Gmail account, Slack, or Discord.",
+    ethereumAddress: "0xb89C33bE71c2aAd77d6712b1AD47274aD9fb7dcb",
+    website: "https://github.com/joyjsmun/chain-concierge",
+    thumbnailImageLink:
+      "https://drive.google.com/open?id=1tOc2dlvaWB1MjGIXy1mf9DcuPJUkiCJI",
+    logoCdnUrl:
+      "https://drive.google.com/open?id=1CzO_IAof1o7SV-X4CD2zL2mPBEv-A8l_",
+    bannerImageLink:
+      "https://drive.google.com/open?id=18fNGhpA2JUgpBbr3Ytc3BPbq94RWiVV1",
+    id: "13",
+    recipientId: 13,
+  },
+  {
+    Timestamp: "9/3/2023 12:56:10",
+    projectName: "RIC Rolling",
+    tagline:
+      "The first rollup-as-a-service platform operated entirely via smart contract. Never gonna rug you up!",
+    description:
+      "Current rollup-as-a-service providers are fully centralised. If they misbehave (by censoring transactions, extracting MEV, or simply interrupting service), there is no recourse for users besides going to court.\nWith our smart contract approach, providers are paid for service provided, and owners (this could be a DAO) remain in control.\nIt's almost as if we could replace paper contracts with smart contracts!",
+    ethereumAddress: "0x2491DbD3C7B198bafbbc3520D048460dF9a61426",
+    website: "https://github.com/ricrolling",
+    thumbnailImageLink:
+      "https://drive.google.com/open?id=1pmoWL-PGh6U1LIZ8_gavam3mjsmFb0wp",
+    logoCdnUrl:
+      "https://drive.google.com/open?id=1LiYEIX2gI7C1hTn6NJQYexgq2HOZFDLq",
+    bannerImageLink:
+      "https://drive.google.com/open?id=1MPZMTo_6M90_sGIDs69WfAk9XX-1PB6W",
+    id: "14",
+    recipientId: 14,
+  },
+  {
+    Timestamp: "9/3/2023 12:57:45",
+    projectName: "Daskalos - \ub2e4\uc2a4\uce7c\ub85c\uc2a4 ",
+    tagline:
+      "Daskalos: An interactive playground for transaction validation, offering Web2 developers a no-code, concept-driven learning experience through incremental gameplay.",
+    description:
+      "With the increasing shift towards Web3, a growing number of Web2 developers are transitioning into the Web3 development ecosystem. Yet, many encounter difficulties in fully understanding Web3 concepts, often resulting in less optimal implementations.\n\nThere's a clear gap between purely theoretical resources that explain Web3 technology and hands-on coding exercises. Addressing this need, Daskalos - \ub2e4\uc2a4\uce7c\ub85c\uc2a4, inspired by the god of education, offers an interactive, game-based playground. This approach, focusing on a no-code learning experience, begins with basic concepts and gradually introduces more intricate systems. The platform's primary aim is to immerse Web2 developers in an experiential understanding of Web3 operations, moving beyond mere definitions or abstract explanations. By doing so, we hope to streamline their transition into Web3 development. Furthermore, as an open-source and free platform, Daskalos ensures accessibility, promoting inclusivity and reaching a diverse audience, regardless of financial barriers.",
+    ethereumAddress: "0x7fBCBd701E1c97aF4aac4248B9F13a661bCFb392",
+    website: "https://github.com/selimerunkut/ethkorea_jinjang_team_repo",
+    thumbnailImageLink:
+      "https://drive.google.com/open?id=1R-Q8cIsk7bhEyLPM36qgU3oHFxnbNH3s",
+    logoCdnUrl:
+      "https://drive.google.com/open?id=1zeG5h9Orl5rLVZ9ZqrzR_pZkxqmDPDv3",
+    bannerImageLink:
+      "https://drive.google.com/open?id=1A6ulgf77dx6T2hTrvsiQhpyq2AUgOv_w",
+    id: "15",
+    recipientId: 15,
+  },
+  {
+    Timestamp: "9/3/2023 13:08:01",
+    projectName: "IDEN",
+    tagline:
+      "IDEN is a mutual voting system designed to help you discover your identity. The goal of IDEN is to evolve into the Identity Layer of Ethereum and set a new standard for identities of web.",
+    description:
+      'IDEN solves the issue in traditional social media where people only show a curated version of themselves, without really knowing how others perceive them. On platforms like Instagram, people create the image they want to portray to others. However, they cannot know how they are actually perceived by others.\nIn IDEN, information about how others view you is continually conveyed through voting. In IDEN app, users are presented with questions about others, such as "Who among these people is the best listener?" along with a list of people they know as options. Users then vote for the person who they think best fits the criteria of the question. Each vote is recorded in individual SBTs (Subjective Blockchain Tokens), and the accumulated voting data is summarized by AI NLP model to dynamically update a single Identity SBT. In this Identity SBT, every user has a summary of their personality evaluted by other users. Through this, users can have fun recording how they actually feel about others, and also hear what other people think about them.',
+    ethereumAddress: "0xD29Df18c8bF98e2cf50376551da68507bf63fF1B",
+    website: "https://github.com/orgs/ethcon-iden/repositories",
+    thumbnailImageLink:
+      "https://drive.google.com/open?id=1cIwmTMSu80NgJPvIuzd-W9YTOemZzEad",
+    logoCdnUrl:
+      "https://drive.google.com/open?id=1KkJBZBxJ6kApJDUVa3-O-_vBTi-9ebRn",
+    bannerImageLink:
+      "https://drive.google.com/open?id=1PSWXr5940h64Ycwv38o9uMh60rLbBUwb",
+    id: "16",
+    recipientId: 16,
+  },
 ];
 
 export function getProjects() {
